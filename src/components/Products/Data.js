50499d29e3b30f558bd54aeae21272e0555ddf5d
antoniobarbato <antonio.barbato95@gmail.com>
2022-09-01 12:33:47 +0200

    firstcommit

---
+++ src/components/Products/Data.js
@@ -0,0 +1,61 @@
+import product1 from '../../Images/pizza-marinara.jpg'
+import product2 from '../../Images/hawaiian-pizza.jpg'
+import product3 from '../../Images/veggie-pizza.jpg'
+import sweet1 from '../../Images/donuts.jpg'
+import sweet2 from '../../Images/vanilla-ice.jpg'
+import sweet3 from '../../Images/brownies.jpg'
+
+export const productData = [
+    {
+        img: product1,
+        alt:'Pizza',
+        name:'Supreme Pizza',
+        desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price:'$19.99',
+        button:'Add to Cart'
+    },
+    {
+        img: product2,
+        alt:'Pizza',
+        name:'Hawaiian Paradise',
+        desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price:'$16.99',
+        button:'Add to Cart'
+    },
+    {
+        img: product3,
+        alt:'Pizza',
+        name:'Veggie Overload',
+        desc:'Marinara sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price:'$14.99',
+        button:'Add to Cart'
+    },
+]
+
+
+export const productDataTwo = [
+    {
+        img: sweet1,
+        alt:'Donuts',
+        name:'Donuts',
+        desc:'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+        price:'$9.99',
+        button:'Add to Cart'
+    },
+    {
+        img: sweet2,
+        alt:'Vanilla Ice Cream',
+        name:'Vanilla Ice Cream',
+        desc:'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+        price:'$12.99',
+        button:'Add to Cart'
+    },
+    {
+        img: sweet3,
+        alt:'Brownies',
+        name:'Brownies',
+        desc:'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+        price:'$9.99',
+        button:'Add to Cart'
+    },
+]
